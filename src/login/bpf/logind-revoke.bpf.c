9d7e182c0cb4e37aa3ec8a9d6811e34dc873316c
Peter Hutterer <peter.hutterer@who-t.net>
2022-05-03 14:32:36 +1000

    Allow BPF in logind

--- src/login/bpf/logind-revoke.bpf.c
+++ src/login/bpf/logind-revoke.bpf.c
@@ -8,81 +8,18 @@
 
 #include <linux/types.h>
 #include <linux/bpf.h>
+#include <linux/hidraw.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
-#include <stdint.h>
-#include <unistd.h>
 
-struct {
-        __uint(type, BPF_MAP_TYPE_HASH);
-        __uint(max_entries, 8192);
-        __type(key, struct hidraw_list *);
-        __type(value, uint8_t);
-} authorized_files SEC(".maps");
+struct hidraw_revoke_syscall_args {
+       int fd;
+};
 
-struct file {
-        void *private_data;
-} __attribute__((preserve_access_index));
-
-int foreground = 1;
-
-SEC("fexit/hidraw_open")
-int BPF_PROG(hidraw_open, struct inode *inode, struct file *file, int ret)
+SEC("syscall")
+int hidraw_revoke(struct hidraw_revoke_syscall_args *args)
 {
-        const uint8_t revoked = 0;
-        struct hidraw_list *list = file->private_data;
-
-        if (ret)
-                return 0;
-
-        /* not in foreground, don't care */
-        if (!foreground)
-                return 0;
-
-        /* store the id in the allowed list */
-        bpf_map_update_elem(&authorized_files, &list, &revoked, BPF_ANY);
-
-        return 0;
-}
-
-SEC("fentry/hidraw_release")
-int BPF_PROG(hidraw_release, struct inode *inode, struct file *file)
-{
-        struct hidraw_list *list = file->private_data;
-
-        /* not in foreground, don't care */
-        if (!foreground)
-                return 0;
-
-        /* the file has been closed, we can clean up */
-        bpf_map_delete_elem(&authorized_files, &list);
-
-        return 0;
-}
-
-static int is_revoked(struct hidraw_list *list)
-{
-        uint8_t *revoked;
-
-        /* first check if the file is in our list */
-        revoked = bpf_map_lookup_elem(&authorized_files, &list);
-
-        /* not part of our list, abort */
-        if (!revoked)
-                return 0;
-
-        /* we are not in foreground, the file is revoked */
-        if (!foreground)
-                return 1;
-
-        /* foreground, let's use the revoked state */
-        return *revoked;
-}
-
-SEC("fmod_ret/hidraw_is_revoked")
-int BPF_PROG(hidraw_bpf_revoked, struct hidraw_list *list)
-{
-        return is_revoked(list);
+       return hid_bpf_hidraw_revoke(args->fd);
 }
 
 char LICENSE[] SEC("license") = "GPL";
